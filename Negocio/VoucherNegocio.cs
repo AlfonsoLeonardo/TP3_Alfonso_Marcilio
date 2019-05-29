3ae3c95ff2c2a574718505e7f551fc3e7bf5994b
AlfonsoLeonardo <alfonso.leonardo.d@iclud.com>
2019-05-29 20:53:01 -0300

    validacion medio hacer

---
+++ Negocio/VoucherNegocio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+
+namespace Negocio
+{
+   public class VoucherNegocio
+    {
+        public bool validarVoucher(string codigo)
+        {
+
+            AccesoDatosManager conexion;
+            try
+            {
+                conexion = new AccesoDatosManager();
+                conexion.setearConsulta("select codigovoucher from vouchers Where codigovoucher=@voucher");
+                conexion.Comando.Parameters.Clear();
+                conexion.Comando.Parameters.AddWithValue("@voucher", codigo);
+                conexion.abrirConexion();
+                conexion.ejecutarConsulta();
+                if (conexion.Lector.Read())
+                {
+                   
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}
+
